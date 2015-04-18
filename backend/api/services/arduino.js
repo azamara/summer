bc334d472d0255025bc86854f8e00ca3ac7ea1e8
William Kim <azamara@me.com>
2015-04-18 21:00:06 +0900

    remove bluetooth serial

--- backend/api/services/arduino.js
+++ backend/api/services/arduino.js
@@ -9,73 +9,31 @@ var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 var Arduino = {
   sensor: function (Model) {
     var _this = this;
+    serialPort = [];
 
-    serialPort = new SerialPort(config.serialPort, {
-      baudrate: 9600,
-      parser: serialPort.parsers.readline('\n')
-    }, false); // this is the openImmediately flag [default is true]
+    _.forEach(config.serialPorts, function (port, i) {
+      serialPort[i] = require('serialport')
+      serialPort[i] = new SerialPort(port, {
+        baudrate: 9600,
+        parser: serialPort[i].parsers.readline('\n')
+      }, false);
 
-    serialPort.open(function (error) {
-      if (error) {
-        console.log('failed to open: ' + error);
-      } else {
-        serialPort.on('data', function (data) {
-          try {
-            var item = JSON.parse(data);
-            console.log('Location ' + item.location + ': ' + data);
-            _this.create(Model, item);
-          } catch (e) {
-            console.log(e);
-          }
-        });
-      }
-    });
-
-    btSerial.on('found', function (address, name) {
-      console.log('Bluetooth device: ' + address, name);
-      if (address === '98-d3-31-60-2d-08') {
-        btSerial.findSerialPortChannel(address, function (channel) {
-          console.log('find bluetooth device: ' + address);
-          btSerial.connect(address, channel, function () {
-            console.log('connected');
-
-            btSerial.write(new Buffer('my data', 'utf-8'), function (err, bytesWritten) {
-              if (err) console.log(err);
-            });
-
-            var result = '';
-            btSerial.on('data', function (buffer) {
-              buffer = buffer.toString('utf-8') || '';
-              result += buffer;
-
-              if (is.include(buffer, '}')) {
-                try {
-                  result = _.trim(result);
-                  var item = JSON.parse(result);
-                  console.log('Location ' + item.location + ': ' + result);
-                  _this.create(Model, item);
-                } catch (e) {
-                  console.log(e);
-                }
-
-                result = '';
-              }
-            });
-          }, function () {
-            btSerial.inquire();
-            console.log('cannot connect');
+      serialPort[i].open(function (error) {
+        if (error) {
+          console.log('failed to open: ' + error);
+        } else {
+          serialPort[i].on('data', function (data) {
+            try {
+              var item = JSON.parse(data);
+              console.log('Location ' + item.location + ': ' + data);
+              _this.create(Model, item);
+            } catch (e) {
+              console.log(e);
+            }
           });
-
-          // close the connection when you're ready
-          btSerial.close();
-        }, function () {
-          btSerial.inquire();
-          console.log('found nothing');
-        });
-      }
+        }
+      });
     });
-
-    btSerial.inquire();
   },
   create: function (Model, item) {
     Model.create(item).exec(function (err, item) {
